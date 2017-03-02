cc8b209e09dc62e23f9869d395fc6373a1afff88
Noah Peden <noahpeden@gmail.com>
2017-03-01 19:56:54 -0700

    homepage styling looks awesome with tile menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import {Link} from "react-router"
 import logo from "./logo.png"
-
+import map from './assets/old-world-map.jpg'
 
 
 const Header = () => {
   return (
     <div className="header-container">
       <header id="header-text"></header>
-      <div id="Container">
-        <img className="logo" src={logo} />
-        <video poster="https://cdn1.goabroad.com/webroot/app/images/intro-video-poster.jpg" id="bgvid" playsInline autoPlay muted loop>
+        <div className="logo-container"><img className="logo" src={logo} /></div>
+        {/* <img src={map} className="map" /> */}
+      <video poster="https://cdn1.goabroad.com/webroot/app/images/intro-video-poster.jpg" id="bgvid" playsInline autoPlay muted loop>
         <source src="https://cdn3.goabroad.com/videos/ga-intro-video.webm" type="video/webm"/>
         <source src="https://cdn4.goabroad.com/videos/ga-intro-video.mp4" type="video/mp4"/>
       </video>
     </div>
-  </div>
 )
 }
 
